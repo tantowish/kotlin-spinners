25a0aec0a15bb3c22bb5ea7b59c16c01528ad8e3
tanto <tantows001@gmail.com>
2023-09-19 23:47:01 +0700

    Logic update

--- app/src/main/java/com/example/kotlin_spinners/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_spinners/MainActivity.kt
@@ -1,19 +1,41 @@
 package com.example.kotlin_spinners
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.DatePicker
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.kotlin_spinners.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var type: Array<String>
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        with(binding){
+        type=resources.getStringArray(R.array.tickets)
 
+        with(binding){
+            val adapterType = ArrayAdapter(this@MainActivity, androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item, type)
+            spinnerTicket.adapter = adapterType
+
+
+            val selectedDate = "${datePicker.dayOfMonth}-${datePicker.month+1}-${datePicker.year}"
+
+            var selectedTime = ""
+            timePicker.setOnTimeChangedListener{
+                    view, hourOfDay, minute ->
+                selectedTime = "$hourOfDay : $minute"
+            }
+            btnPesan.setOnClickListener{
+                val toastValue = "Ticket ${spinnerTicket.selectedItem.toString()} $selectedDate $selectedTime has been ordered"
+                Toast.makeText(this@MainActivity, toastValue, Toast.LENGTH_SHORT).show()
+            }
         }
 
 
