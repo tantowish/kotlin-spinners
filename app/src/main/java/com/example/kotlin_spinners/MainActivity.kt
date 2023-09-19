3bf2f687dd00f12af16d1390bbbf55eb569948a2
tanto <tantows001@gmail.com>
2023-09-19 14:01:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_spinners/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin_spinners
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.DatePicker
+import com.example.kotlin_spinners.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+
+        }
+
+
+    }
+}
